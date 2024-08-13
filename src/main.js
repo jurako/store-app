c5636cf8acb8b65dd96a13f3d9269f1c4a8ed50b
Jurako <jurako91@gmail.com>
2024-08-13 20:27:03 +0300

    Add axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
+axios.defaults.baseURL = 'https://api.escuelajs.co/api/v1'
 
 import App from './App.vue'
 import router from './router'
