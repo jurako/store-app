3d0d99df9ffbad694754b2860b23b9e62c41063c
Jurako <jurako91@gmail.com>
2024-09-21 09:24:18 +0300

    add sign in view

---
+++ src/components/form_items/InputField.vue
@@ -0,0 +1,14 @@
+<template>
+    <input type="text"
+      :value="modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
+      
+      :placeholder="placeholder"/>
+</template>
+
+<script setup>
+
+const emit = defineEmits(['update:modelValue']);
+const props = defineProps(['modelValue', 'placeholder']);
+
+</script>
--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="w-96 rounded-xl border bg-white p-8 shadow sm:flex-grow sm:w-auto sm:max-w-screen-sm">
         <h1 class="text-center text-3xl font-bold text-neutral-600">Sign in</h1>
+        <section class="flex flex-col items-center mt-11 mb-7 gap-y-5">
+          <InputField class="w-full sm:w-1/2" v-model="email" placeholder="Email"/>
+          <InputField class="w-full sm:w-1/2" v-model="password" placeholder="Password"/>
+          <BaseButton class="w-full sm:w-1/2">LOGIN</BaseButton>
+        </section>
     </div>
 </template>
+
+<script setup>
+import InputField from '@/components/form_items/InputField.vue';
+import BaseButton from '@/components/BaseButton.vue';
+import { ref } from 'vue'
+
+let email = ref('');
+let password = ref('');
+
+</script>
