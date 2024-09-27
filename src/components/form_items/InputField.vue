5d6c9418f0a29b037e2349f1fae370c080c6b1fd
Jurako <jurako91@gmail.com>
2024-09-27 22:27:57 +0300

    make type attribute dynamic

--- src/components/form_items/InputField.vue
+++ src/components/form_items/InputField.vue
@@ -1,14 +1,20 @@
 <template>
-    <input type="text"
-      :value="modelValue"
-      @input="emit('update:modelValue', $event.target.value)"
-      
-      :placeholder="placeholder"/>
+  <input
+    :type="inputType"
+    :value="modelValue"
+    @input="emit('update:modelValue', $event.target.value)"
+    :placeholder="placeholder"
+  />
 </template>
 
 <script setup>
-
-const emit = defineEmits(['update:modelValue']);
-const props = defineProps(['modelValue', 'placeholder']);
-
+const emit = defineEmits(['update:modelValue'])
+const props = defineProps({
+ modelValue: String,
+ placeholder: String,
+ inputType: {
+    type: String,
+    default: 'text'
+ }
+});
 </script>
