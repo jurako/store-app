22defa03b047675b26589953ad3ac0bcf7fa8545
Jurako <jurako91@gmail.com>
2024-09-29 22:54:50 +0300

    add radio button component

---
+++ src/components/form_items/RadioButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <input
+    type="radio"
+    :id="value"
+    :name="name"
+    :value="value"
+    :checked="value == modelValue"
+    @change="() => $emit('update:modelValue')"
+  />
+  <label :for="value">{{ value }}</label>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  value: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: String,
+    default: ''
+  }
+})
+</script>
