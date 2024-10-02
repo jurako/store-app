b9197dc06b94d8c2e013c32f35ea9163d0cae2be
Jurako <jurako91@gmail.com>
2024-10-02 22:05:22 +0300

    reformat code

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -1,7 +1,7 @@
-import Validator from "./validator";
+import Validator from './validator'
 
 function isObjectEmpty(obj) {
-    return obj && Object.keys(obj).length == 0 && obj.constructor == Object;
+  return obj && Object.keys(obj).length == 0 && obj.constructor == Object
 }
 
 export { Validator, isObjectEmpty }
--- src/misc/validator.js
+++ src/misc/validator.js
@@ -1,57 +1,51 @@
 class Validator {
-    #EMAIL_REG_EXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    #ERRORS = {
-        required:   'Please fill out this field',
-        invalidEmail: 'Please enter a valid email address'
-    }
+  #EMAIL_REG_EXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  #ERRORS = {
+    required: 'Please fill out this field',
+    invalidEmail: 'Please enter a valid email address'
+  }
 
-    errorClasses = ['border-red-600', 'border-2'];
+  errorClasses = ['border-red-600', 'border-2']
 
-
-    /*
+  /*
         {
             'required': {ref1: ref1, ref2: ref2,...refn: refn},
             'email': {ref1: ref1},
             ...
         }
     */
-    constructor(rules) {
-        this.errors = {};
-        this.rules = rules;
+  constructor(rules) {
+    this.errors = {}
+    this.rules = rules
+  }
+
+  validate() {
+    for (const [rule, refs] of Object.entries(this.rules)) {
+      switch (rule) {
+        case 'required':
+          this.#dispatch('isEmpty', refs, 'required')
+          break
+        case 'email':
+          this.#dispatch('isInvalidEmail', refs, 'invalidEmail')
+          break
+      }
     }
+  }
 
-    validate() {
-        for(const [rule, refs] of Object.entries(this.rules)) {
-
-            switch(rule) {
-                case 'required':
-                    this.#dispatch('isEmpty', refs, 'required');
-                break;
-                case 'email':
-                    this.#dispatch('isInvalidEmail', refs, 'invalidEmail');
-                break;
-            }
-
-        }
+  #dispatch(method, refs, errMsg) {
+    for (const [property, ref] of Object.entries(refs)) {
+      if (this[method](ref.value)) {
+        this.errors[property] = errMsg
+      }
     }
-    
-    #dispatch(method, refs, errMsg) {
-        for(const [property, ref] of Object.entries(refs)) {
+  }
 
-            if(this[method](ref.value)) {
-                this.errors[property] = errMsg;
-            }
-
-        }
-
-    }
-
-    isEmpty(value) {
-        return !value;
-    }
-    isInvalidEmail(value) {
-        return !this.#EMAIL_REG_EXP.test(value)
-    }
+  isEmpty(value) {
+    return !value
+  }
+  isInvalidEmail(value) {
+    return !this.#EMAIL_REG_EXP.test(value)
+  }
 }
 
-export default Validator;
+export default Validator
