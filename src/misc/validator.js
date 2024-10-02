9c8ee5f41cd7436c7cdda441623277a8225fbc0c
Jurako <jurako91@gmail.com>
2024-10-02 22:03:56 +0300

    finish validator class

--- src/misc/validator.js
+++ src/misc/validator.js
@@ -1,14 +1,55 @@
 class Validator {
-    static #EMAIL_REG_EXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    static ERRORS = {
-        EMPTY_FIELD:   'Please fill out this field',
-        INVALID_EMAIL: 'Please enter a valid email address'
+    #EMAIL_REG_EXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    #ERRORS = {
+        required:   'Please fill out this field',
+        invalidEmail: 'Please enter a valid email address'
     }
-    static errorClasses = ['border-red-600', 'border-2'];
-    static isEmpty(value) {
+
+    errorClasses = ['border-red-600', 'border-2'];
+
+
+    /*
+        {
+            'required': {ref1: ref1, ref2: ref2,...refn: refn},
+            'email': {ref1: ref1},
+            ...
+        }
+    */
+    constructor(rules) {
+        this.errors = {};
+        this.rules = rules;
+    }
+
+    validate() {
+        for(const [rule, refs] of Object.entries(this.rules)) {
+
+            switch(rule) {
+                case 'required':
+                    this.#dispatch('isEmpty', refs, 'required');
+                break;
+                case 'email':
+                    this.#dispatch('isInvalidEmail', refs, 'invalidEmail');
+                break;
+            }
+
+        }
+    }
+    
+    #dispatch(method, refs, errMsg) {
+        for(const [property, ref] of Object.entries(refs)) {
+
+            if(this[method](ref.value)) {
+                this.errors[property] = errMsg;
+            }
+
+        }
+
+    }
+
+    isEmpty(value) {
         return !value;
     }
-    static isInvalidEmail(value) {
+    isInvalidEmail(value) {
         return !this.#EMAIL_REG_EXP.test(value)
     }
 }
