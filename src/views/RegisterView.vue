dd4bfe799762dceeccfc0b421f15ccc28c44f415
Jurako <jurako91@gmail.com>
2024-10-05 00:35:40 +0300

    add field validation

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,22 +3,43 @@
     <h1 class="text-center text-3xl font-bold text-neutral-600">Registration Form</h1>
     <section class="mb-7 mt-11 space-y-6">
       <div class="flex flex-col gap-y-6 sm:flex-row sm:items-center sm:gap-x-6">
-        <InputField @input="setGenderToM()" class="w-full sm:w-1/2" placeholder="First Name" />
-        <InputField @input="setGenderToF()" class="w-full sm:w-1/2" placeholder="Last Name" />
+        <InputField
+          v-model="firstName"
+          :class="[errors.firstName ? Validator.errorClasses : '']"
+          class="w-full sm:w-1/2"
+          placeholder="First Name"
+        />
+        <InputField
+          v-model="lastName"
+          :class="[errors.lastName ? Validator.errorClasses : '']"
+          class="w-full sm:w-1/2"
+          placeholder="Last Name"
+        />
       </div>
       <div class="flex flex-col gap-y-6 sm:flex-row sm:items-center sm:gap-x-6">
-        <InputField type="date" class="w-full sm:w-1/2" placeholder="Birthday" />
+        <InputField v-model="birthday" type="date" class="w-full sm:w-1/2" placeholder="Birthday" />
         <div class="w-full sm:w-1/2">
-          <h2 class="font-semibold mb-1 sm:mb-0">Gender:</h2>
-          <RadioButton class="mr-2" v-model="gender" name="gender" value="Female" />
-          <RadioButton v-model="gender" name="gender" value="Male" />
+          <h2 class="mb-1 font-semibold sm:mb-0">Gender:</h2>
+          <RadioButton v-model="gender" name="gender" value="F" label="Female" class="mr-2" />
+          <RadioButton v-model="gender" name="gender" value="M" label="Male" />
         </div>
       </div>
       <div class="flex flex-col gap-y-6 sm:flex-row sm:items-center sm:gap-x-6">
-        <InputField class="w-full sm:w-1/2" placeholder="Email" />
-        <InputField class="w-full sm:w-1/2" placeholder="Phone Number" />
+        <InputField
+          v-model="email"
+          :class="[errors.email ? Validator.errorClasses : '']"
+          class="w-full sm:w-1/2"
+          placeholder="Email"
+        />
+        <InputField
+          v-model="password"
+          type="password"
+          :class="[errors.password ? Validator.errorClasses : '']"
+          class="w-full sm:w-1/2"
+          placeholder="Password"
+        />
       </div>
-      <select class="w-full" name="country">
+      <select v-model="country" class="w-full" name="country">
         <option value="Latvia">Latvia</option>
         <option value="Estonia">Estonia</option>
         <option value="Lithuania">Lithuania</option>
@@ -26,9 +47,7 @@
       <ErrorMessage v-show="!isObjectEmpty(errors)">
         {{ errors.firstName || errors.lastName || errors.email || errors.password }}
       </ErrorMessage>
-      <BaseButton class="uppercase">
-        Submit
-      </BaseButton>
+      <BaseButton @click="submit" class="uppercase"> Submit </BaseButton>
     </section>
   </div>
 </template>
@@ -37,8 +56,33 @@
 import InputField from '@/components/form_items/InputField.vue'
 import RadioButton from '@/components/form_items/RadioButton.vue'
 import ErrorMessage from '@/components/form_items/ErrorMessage.vue'
-import BaseButton from '@/components/BaseButton.vue';
+import BaseButton from '@/components/BaseButton.vue'
+import BaseButton from '@/components/BaseButton.vue'
 import { ref } from 'vue'
+import { isEmpty, isInvalidEmail, Validator, isObjectEmpty } from '@/misc/helpers'
 
-const gender = ref('Female')
+const firstName = ref('')
+const lastName = ref('')
+const birthday = ref(0)
+const gender = ref('F')
+const email = ref('')
+const password = ref('')
+const country = ref('')
+const errors = ref({})
+
+const validator = new Validator([
+  { ref: firstName, errorField: 'firstName', rules: [isEmpty] },
+  { ref: lastName, errorField: 'lastName', rules: [isEmpty] },
+  { ref: email, errorField: 'email', rules: [isEmpty, isInvalidEmail] },
+  { ref: password, errorField: 'password', rules: [isEmpty] }
+])
+
+function submit() {
+  validator.validate()
+  errors.value = validator.errors
+
+  if (isObjectEmpty(errors.value)) {
+    alert('Good to submit register form!')
+  }
+}
 </script>
