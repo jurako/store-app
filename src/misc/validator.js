7085413324c2538158ed393c83a3f816539293ea
Jurako <jurako91@gmail.com>
2024-10-05 00:17:04 +0300

    reformat code

--- src/misc/validator.js
+++ src/misc/validator.js
@@ -1,69 +1,66 @@
 class Validator {
-
   static errorClasses = ['border-red-600', 'border-2']
 
   constructor(items) {
-    this.#validateArgument(items);
+    this.#validateArgument(items)
 
     this.errors = {}
     this.items = items
   }
 
   validate() {
-    this.errors = {};
+    this.errors = {}
 
-    for(const item of this.items) {
-      for(const rule of item.rules) {
-        if(rule.fails(item.ref.value)) {
-          this.errors[item.errorField] = rule.errorMsg;
+    for (const item of this.items) {
+      for (const rule of item.rules) {
+        if (rule.fails(item.ref.value)) {
+          this.errors[item.errorField] = rule.errorMsg
         }
       }
     }
   }
 
   #validateArgument(rules) {
-    if(rules?.constructor?.name != 'Array')
-      throw new Error('Passed value is not an Array');
+    if (rules?.constructor?.name != 'Array') throw new Error('Passed value is not an Array')
 
-    if(!rules.length)
-      throw new Error('Validator must contain atleast one validation item');
+    if (!rules.length) throw new Error('Validator must contain atleast one validation item')
 
-    for(const validationObj of rules) {
+    for (const validationObj of rules) {
+      if (validationObj?.constructor?.name != 'Object')
+        throw new Error('Invalid validation item value, must be an Object')
 
-      if(validationObj?.constructor?.name != 'Object')
-        throw new Error('Invalid validation item value, must be an Object');
-
-      if(!validationObj.hasOwnProperty('value') ||
-         !validationObj.hasOwnProperty('rules') ||
-         !validationObj.hasOwnProperty('errorField')
+      if (
+        !validationObj.hasOwnProperty('value') ||
+        !validationObj.hasOwnProperty('rules') ||
+        !validationObj.hasOwnProperty('errorField')
       )
-        throw new Error('Validation object must contain value, errorField and rules properties');
+        throw new Error('Validation object must contain value, errorField and rules properties')
     }
   }
 }
 
 class ValidationRule {
   fails(value) {
-    throw new Error('This method must be overridden by subclasses');
+    throw new Error('This method must be overridden by subclasses')
   }
 }
 
 class isEmptyRule extends ValidationRule {
-  errorMsg = 'Please fill out this field';
+  errorMsg = 'Please fill out this field'
 
   fails(value) {
-    return !value;
+    return !value
   }
 }
 
 class isInvalidEmailRule extends ValidationRule {
-  static #emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  errorMsg = 'Please enter a valid email address';
+  static #emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  errorMsg = 'Please enter a valid email address'
 
   fails(value) {
     return !isInvalidEmailRule.#emailRegExp.test(value)
   }
 }
-export const isEmpty = new isEmptyRule();
-export const isInvalidEmail = new isInvalidEmailRule();
-export { Validator };
+export const isEmpty = new isEmptyRule()
+export const isInvalidEmail = new isInvalidEmailRule()
+export { Validator }
